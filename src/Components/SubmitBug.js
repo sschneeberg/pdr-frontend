a1d60c8fcfd8055e26a2876f6a72477bb3323442
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-12-21 13:55:06 -0600

    Add Flow of pages between eachother

--- src/Components/SubmitBug.js
+++ src/Components/SubmitBug.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 function SubmitBug(props) {
 
   const onSubmitHandler = (e) => {
       e.preventDefault();
-      
+      props.history.push('/sbpt2')
   }
 
     return (
@@ -13,7 +14,7 @@ function SubmitBug(props) {
         <form action="">
           <label for="text">image</label>
           <input type="text" placeholder='Image submit' />
-          <input type="submit" name="" id="" />
+          <input type="submit" name="" id="" onClick={onSubmitHandler} />
         </form>
         <form action="">
           <label for="text">text</label>
