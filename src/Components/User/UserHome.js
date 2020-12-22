6f70bd10e2edbacb7b11cc31c1340b6205a332be
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-22 15:28:00 -0500

    passport working w/ front end

--- src/Components/User/UserHome.js
+++ src/Components/User/UserHome.js
@@ -1,40 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import axios from 'axios';
 
 class UserHome extends Component {
     constructor(props) {
-        super(props)
-        this.state ={
+        super(props);
+        this.state = {
             description: [],
             date: [],
             website: [],
             status: [],
             image: [],
             userId: ''
-        }
+        };
     }
-
-
 
     componentDidMount() {
         console.log(this.props.user);
-        axios.get(`http://localhost:8000/api/users/current`)
-        .then(({ data }) => console.log(data))
-        .catch(e => {
-            console.log(e);
-        })
-
+        axios
+            .get(`http://localhost:8000/api/dashboard`)
+            .then(({ data }) => console.log(data))
+            .catch((e) => {
+                console.log(e);
+            });
     }
 
     render() {
-        return(
-        <div>
-            
-        </div>
-        );
+        return <div></div>;
     }
 }
-
-
 
 export default UserHome;
